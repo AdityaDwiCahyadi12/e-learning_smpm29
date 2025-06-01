3e80ef10aa1ed569eba78a178c5a4d28f72ca744
AdityaDwiCahyadi12 <adityadwicahyadi12@gmail.com>
2025-06-01 18:56:14 +0700

    perbaikin database

--- app/config/database.php
+++ app/config/database.php
@@ -10,7 +10,14 @@ try {
     $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
     $pdo = new PDO($dsn, $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Koneksi gagal: " . $e->getMessage());
+}catch (PDOException $e) {
+    error_log("Database connection error: " . $e->getMessage()); // log ke Railway
+    die("Terjadi kesalahan pada database: " . $e->getMessage()); // tampilkan ke browser
 }
+
+error_log("ENV DEBUG - MYSQLHOST: " . getenv('MYSQLHOST'));
+error_log("ENV DEBUG - MYSQLPORT: " . getenv('MYSQLPORT'));
+error_log("ENV DEBUG - MYSQL_DATABASE: " . getenv('MYSQL_DATABASE'));
+error_log("ENV DEBUG - MYSQLUSER: " . getenv('MYSQLUSER'));
+error_log("ENV DEBUG - MYSQL_ROOT_PASSWORD: " . getenv('MYSQL_ROOT_PASSWORD'));
 ?>
